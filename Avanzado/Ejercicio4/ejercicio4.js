4c66156c2f0bb5dd90506838800e39718b188eb0
Andrea <amartin.onda@gmail.com>
2021-12-15 13:41:31 +0100

    cambios15Dic

---
+++ Avanzado/Ejercicio4/ejercicio4.js
@@ -0,0 +1,76 @@
+//NO FUNCIONA
+
+window.onload = function(){
+
+    var XMLHttpRequestObject = false;
+
+    if (window.XMLHttpRequest) {
+
+        XMLHttpRequestObject = new XMLHttpRequest();
+    }
+
+    document.getElementById("comprobar").addEventListener("click", ()=>{
+
+        sacardatos(XMLHttpRequestObject);
+
+    });
+
+}
+
+function sacardatos(XMLHttpRequestObject){
+
+    if(XMLHttpRequestObject){
+
+        var lugar = document.getElementById("contenedor");
+		
+        XMLHttpRequestObject.open("POST", "script.php", true);
+
+		XMLHttpRequestObject.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+          
+        XMLHttpRequestObject.onreadystatechange = ()=>{
+
+            if(XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200){
+                
+                var raiz1 = document.getElementsByTagName("respuesta");
+                raiz = raiz1[0];
+                
+                var disponible = raiz.getElementsByTagName("disponible")[0].firstChild.nodeValue;
+                
+                lugar.innerHTML = XMLHttpRequestObject.responseText;
+
+                if(disponible == "si") {
+                    
+                    document.getElementById("contenedor").innerHTML = "El nombre elegido ["+login+"] est� disponible";
+                
+                }
+                else {
+                   
+                    var mensaje = "NO está disponible el nombre elegido ["+login+"]. Puedes probar con las siguientes alternativas.";
+                    
+                    var alternativas = raiz.getElementsByTagName("alternativas")[0];
+                    
+                    var logins = alternativas.getElementsByTagName("login");
+                    
+                    mensaje += "<ul>";
+                    
+                    for(var i=0; i<logins.length; i++) {
+                    
+                        mensaje += "<li><a href=\"#\" onclick=\"selecciona('"+logins[i].firstChild.nodeValue+"'); return false\">"+logins[i].firstChild.nodeValue+"<\/a><\/li>";	
+                    
+                    }
+                    
+                    mensaje += "<\/ul>";
+                    
+                    document.getElementById("contenedor").innerHTML = mensaje;
+                }
+            }
+
+            }
+            
+        }
+
+        XMLHttpRequestObject.send();
+    }
+
+
+
